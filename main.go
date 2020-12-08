62005d31f35116793ab331ca7a6d9975fc3f0b2a
Luciano Lupo <lucianolupo97@gmail.com>
2020-12-08 14:43:00 -0300

    add POST

--- main.go
+++ main.go
@@ -3,10 +3,47 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 )
+type task struct {
+	Id      int    `json:"Id"`
+	Name    string `json:"Name"`
+	Content string `json:"Content"`
+}
+
+type allTasks []task
+
+var tasks = allTasks{
+	{
+		Id:      1,
+		Name:    "Task One",
+		Content: "Some Content",
+	},
+}
+
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Insert a Valid Task Data")
+	}
+	fmt.Println(newTask,reqBody)
+
+
+	json.Unmarshal(reqBody, &newTask)
+	newTask.Id = len(tasks) + 1
+	tasks = append(tasks, newTask)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+
+}
 
 func indexRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "GO API! test")
@@ -17,6 +54,8 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", indexRoute)
+	router.HandleFunc("/tasks", createTask).Methods("POST")
+
 
 
 	log.Fatal(http.ListenAndServe(":3000", router))
